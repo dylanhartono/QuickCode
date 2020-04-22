c5ed482ca30b58c3b5ab206cd8af07c1cfe2a866
hdylan1 <hdylan1@uw.edu>
2020-04-21 21:13:31 -0700

    added Ben's and Andrea's Possible Solution

--- 4-21-2020/Solution.java
+++ 4-21-2020/Solution.java
@@ -1,5 +1,9 @@
-public class Solution {
+// Guys realize all of these are in O(n) runtime, so any of your solutions would have been
+// efficient and fast in this case
 
+public class Solution {
+    // Nice Aaron and Mahin I honestly did not think this would end up being correct, but looking
+    // at the logic, it's all there!
     public static int solve(int days, int[] buses) {
         int multiples = buses[0];
         while(multiples <= days) {
@@ -23,7 +27,37 @@ public class Solution {
         return multiples;
     }
 
-    public static int solve2(int days, int[] planes) {
+    // Ben's supposed solution in fruition. It was sooo close
+    public static int solve2(int days, int[] buses) {
+        int val = buses[buses.length - 1];
+        while(buses[buses.length - 1] <= days - buses[buses.length - 1]) {
+            buses[buses.length - 1] += val;
+        }
+        for(int i = buses.length - 2; i > -1; i--) {
+            val = buses[i];
+            while(buses[i] <= buses[i + 1] - val) {
+                buses[i] += val;
+            }
+        }
+        return buses[0];
+    }
+
+    // Andrea's Solution
+    // If I were following you {3, 7, 2}, 10 -> would end up to be like {1, 3, 0}, 10
+    // Realize if you subtracted all the elements in the array from 10 you would get 6
+    // aka the answer
+    public static int solve3(int days, int[] buses) {
+        for(int i = 0; i < buses.length; i++) {
+            buses[i] = days % buses[i];
+        }
+        for(int i = 0; i < buses.length; i++) {
+            days -= buses[i];
+        }
+        return days;
+    }
+
+    // Do answers like above. Only pure luck will get you to 3 line answers like these
+    public static int solve4(int days, int[] planes) {
         for(int i = planes.length - 1; i > -1; i--) {
             days -= days % planes[i];
         }
