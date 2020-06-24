e0f05171b628e6844ad9245567fd2129b63d0e4c
hdylan1 <hdylan1@uw.edu>
2020-06-23 18:55:12 -0700

    adding solution template'

---
+++ Solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int cases = in.nextInt();
+        for(int i = 1; i <= cases; i++) {
+            ___ sol = solve(in);
+            System.out.println("Case #" + i + ": " + sol);
+        }
+        in.close();
+    }
+
+    public static ___ solve(Scanner in) {
+        return ___;
+    }
+}
